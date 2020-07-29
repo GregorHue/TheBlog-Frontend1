cc01fe067dedb1e1666e5aff54a779f7a51ea465
Gregor Hueltenschmidt <gregor.hueltenschmidt@web.de>
2020-07-29 13:30:21 +0200

    Added interface User. closes #15

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,24 @@
+import { Gender } from './gender';
+
+export interface User {
+  createdAt: Date;
+  lastUpdatedAt: Date;
+  username: string;
+  password: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  gender: Gender;
+  city: string;
+  street: string;
+  houseNumber: number;
+  postalCode: string;
+  user_url: string;
+  posts_url: string;
+  deletedTs: Date;
+}
+
+export interface UserDtoList {
+  users: User[];
+}
+
