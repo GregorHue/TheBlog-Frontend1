d892a4e0e283d78dc45702fcec320ca9045db9de
Gregor Hueltenschmidt <gregor.hueltenschmidt@web.de>
2020-07-30 12:05:18 +0200

    Implemented component users, closes #11

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../interfaces/user';
+import { UserService } from '../../services/user.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-users',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
+
+  constructor(private userService: UserService, public modal: NgbModal) { }
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe(users => this.users = users, error => console.log(error));
   }
+
+  validate = (string: string) => {
+    if (string && string !== '') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  getCompleteAddress(user: User) {
+    const { houseNumber, street, city, postalCode } = user;
+    let address = '';
+    if (this.validate(houseNumber.toString())) {
+      address += houseNumber;
+    }
+    if (this.validate(street)) {
+      if (this.validate(address)) {
+        address += ', ';
+      }
+      address += street;
+    }
+    if (this.validate(city)) {
+      if (this.validate(address)) {
+        address += ', ';
+      }
+      address += city;
+    }
+    if (this.validate(postalCode)) {
+      if (this.validate(address)) {
+        address += ', ';
+      }
+      address += postalCode;
+    }
+    return address;
+  }
+
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { BASEURL } from '../utils/baseUrl';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
 
 
 @Injectable({
@@ -12,6 +13,14 @@ import { map } from 'rxjs/operators';
 export class UserService {
 
   private http: HttpClient;
+  token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsInVzZXJfdXJsIjoiL3VzZXJzLzIiLCJleHAiOjE1OTY4ODk2OTQsImF1dGhvcml0aWVzIjoiUk9MRV9BRE1JTiJ9.uprOL6LU1Y0EueXvuAGuMpZvQVzC7NUknGW9wno5iD5t46TzzF9lDPOdx40KHwKI6ERpXlTmBzIyV4UDpLrZBA'
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': this.token
+    })
+  };
 
   constructor(http: HttpClient) {
     this.http = http;
@@ -20,12 +29,12 @@ export class UserService {
 
   getUsers(): Observable<User[]> {
 
-    return this.http.get<UserDtoList>(`${BASEURL}/users`).pipe(map(list => list.users));
+    return this.http.get<UserDtoList>(`${BASEURL}/users`, this.httpOptions).pipe(map(list => list.users));
 
   }
 
   getUser(userId: number): Observable<User> {
 
-    return this.http.get<User>(`${BASEURL}/users/${userId}`)
+    return this.http.get<User>(`${BASEURL}/users/${userId}`, this.httpOptions)
   }
 }
