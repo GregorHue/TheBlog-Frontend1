81c78af34f6f4c9e57fa4e0628e6e8287550b7bc
Gregor Hueltenschmidt <gregor.hueltenschmidt@web.de>
2020-09-01 19:16:00 +0200

    Added smooth scroll bar, closes #35

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import OverlayScrollbars from 'overlayscrollbars';
+
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +12,13 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+OverlayScrollbars(document.body, {
+  nativeScrollbarsOverlaid: {
+    initialize: false
+  },
+  scrollbars: {
+    autoHide: 'never',
+    clickScrolling: true
+  }
+});
